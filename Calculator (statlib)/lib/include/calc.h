ed200690c5cf8307a476b2c4ed45f0a77d5657b7
Владислав <vladislav.ip514@gmail.com>
2020-08-02 12:55:01 +0700

    Реализована проверка входных данных и внесены изменения в библиотеку

--- Calculator (statlib)/lib/include/calc.h
+++ Calculator (statlib)/lib/include/calc.h
@@ -1,8 +1,6 @@
 #ifndef CALC
 #define CALC
 
-void menu();
-void input_nums(int *, int *);
 int add(int, int);
 int diff(int, int);
 int mul(int, int);
--- Calculator (statlib)/src/main.c
+++ Calculator (statlib)/src/main.c
@@ -16,8 +16,34 @@ int getch() {
     return ch;
 }
 
-void clr_input_buf() {    
+void clear_buffer() {    
     while (getchar() != '\n');
+}
+
+void input_nums(int *a, int *b) {
+    system("clear");
+    printf("Введите первое число: ");
+    while (scanf("%d", a) != 1) {
+        system("clear");
+        printf("Введите первое число: ");
+        clear_buffer();
+    }
+    printf("Введите второе число: ");
+    while (scanf("%d", b) != 1) { 
+        system("clear");
+        printf("Введите второе число: ");
+        clear_buffer();
+    }
+    clear_buffer();
+}
+
+void menu() {
+    system("clear");
+    printf("1. Сложение двух чисел\n");
+    printf("2. Разность двух чисел\n");
+    printf("3. Умножение двух чисел\n");
+    printf("4. Деление двух чисел\n");
+    printf("5. Выход из программы\n");
 }
 
 int main() {
@@ -28,26 +54,23 @@ int main() {
         switch (ch) {
             case 49: {
                 input_nums(&a, &b);
-                clr_input_buf();
-                printf("Результат: %d\n", add(a, b));
+                printf("Результат: %d + %d = %d\n", a, b, add(a, b));
                 getch();
             } break;
             case 50: {
                 input_nums(&a, &b);
-                clr_input_buf();
-                printf("Результат: %d\n", diff(a, b));
+                printf("Результат: %d - %d = %d\n", a, b, diff(a, b));
                 getch();
             } break;
             case 51: {
                 input_nums(&a, &b);
-                clr_input_buf();
-                printf("Результат: %d\n", mul(a, b));
+                printf("Результат: %d * %d = %d\n", a, b, mul(a, b));
                 getch();
             } break;
             case 52: {
                 input_nums(&a, &b);
-                clr_input_buf();
-                printf("Результат: %d\n", divide(a, b));
+                if (b == 0) printf("Деление на ноль невозможно!\n");
+                else printf("Результат: %d / %d = %d\n", a, b, divide(a, b));
                 getch();
             } break;
             case 53: {
