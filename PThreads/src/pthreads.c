9fa724783cde37d25da952a836c0539954666377
Владислав <vladislav.ip514@gmail.com>
2020-08-10 14:48:31 +0700

    Добавлены мьютексы

--- PThreads/src/pthreads.c
+++ PThreads/src/pthreads.c
@@ -4,18 +4,22 @@
 #define CNT_THREADS 1000
 
 long long int num = 0;
+pthread_mutex_t mutex;
 
 void *increment_num(void *args) {
     long long int tmp;
+    pthread_mutex_lock(&mutex);
     for (int i = 0; i < 1000000; i++) {
         tmp = num;
         tmp++;
         num = tmp;
     }
+    pthread_mutex_unlock(&mutex);
 }
 
 int main() {
     pthread_t tid[CNT_THREADS];
+    pthread_mutex_init(&mutex, NULL);
     for (int i = 0; i < CNT_THREADS; i++) {
         pthread_create(&tid[i], NULL, increment_num, NULL);
     }
